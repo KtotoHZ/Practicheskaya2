70e6b8c3748b554ce1a4e9f137934bc7b0b4a51c
Nickita <Nickita@DESKTOP-VMDB76S>
2024-02-29 19:40:34 +0300

    Добавьте файлы проекта.

---
+++ Practicheskaya2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practicheskaya2
+{
+   
+    public partial class MainWindow : Window
+    {
+        List<Person> people = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Users.ItemsSource = people;
+        }
+
+        private void Registr(object sender, RoutedEventArgs e)
+        {
+            if (firstPassword.Text == secondPassword.Text)
+            {
+                DateTime? selectedDate = birthday.SelectedDate;
+                //people.Add(new Person(firstName.Text, lastName.Text, patronymic.Text, selectedDate.Value.ToShortDateString()));
+                people.Add(new Person { FirstName = firstName.Text, LastName = lastName.Text, Patronymic = patronymic.Text, Birthday = selectedDate.Value.Date.ToShortDateString() });
+                Users.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Пароли не совпадают");
+            }
+        }
+    }
+}
---
+++ Practicheskaya2/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicheskaya2
+{
+    internal class Person
+    {
+        public string FirstName {  get; set; }
+        public string LastName {  get; set; }
+        public string Patronymic {  get; set; }
+        public string Birthday {  get; set; }
+
+        //public Person(string firstName, string lastName, string patronymic, string birthday)
+        //{
+        //    FirstName = firstName;
+        //    LastName = lastName;
+        //    Patronymic = patronymic;
+        //    Birthday = birthday;
+        //}
+    }
+}
